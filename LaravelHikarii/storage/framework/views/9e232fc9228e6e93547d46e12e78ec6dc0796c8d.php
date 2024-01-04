6a2d98f93f67a924d5981fceef4519768506a0b0
KyleNT <nes.wil.con64@gmail.com>
2024-01-04 13:29:01 -0300

    Add files via upload

---
+++ LaravelHikarii/storage/framework/views/9e232fc9228e6e93547d46e12e78ec6dc0796c8d.php
@@ -0,0 +1,2 @@
+<h2>Nome: <?php echo e($nome); ?></h2>
+<h2>E-mail: <?php echo e($email); ?></h2><?php /**PATH C:\xampp\htdocs\ClientesLaravelHikarii\resources\views/cliente_single.blade.php ENDPATH**/ ?>
